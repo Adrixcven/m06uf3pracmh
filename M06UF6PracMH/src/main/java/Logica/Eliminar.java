4102352738c3d07f69e1b36292e7b8fab0191f69
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2023-04-20 19:55:37 +0200

    eliminar comentada en formato javadoc

--- M06UF6PracMH/src/main/java/Logica/Eliminar.java
+++ M06UF6PracMH/src/main/java/Logica/Eliminar.java
@@ -14,8 +14,24 @@ import org.bson.Document;
  *
  * @author Adrix
  */
+/**
+ *
+ * Clase que contiene un método estático para eliminar un repositorio de una
+ * base de datos MongoDB.
+ */
 public class Eliminar {
 
+    /**
+     *
+     * Método que elimina un repositorio de una base de datos MongoDB.
+     *
+     * @param in Objeto de la clase Scanner para leer datos desde la consola.
+     * @param coleccio Objeto de la clase MongoCollection que representa la
+     * colección a la que pertenece el repositorio.
+     * @param bbdd Objeto de la clase MongoDatabase que representa la base de
+     * datos donde se encuentra el repositorio.
+     * @param repositorio Nombre del repositorio a eliminar.
+     */
     public static void eliminarRepositorio(Scanner in, MongoCollection<Document> coleccio, MongoDatabase bbdd, String repositorio) {
         boolean repetir = true;
         coleccio = bbdd.getCollection(repositorio);
@@ -29,4 +45,3 @@ public class Eliminar {
         }
     }
 }
-
--- M06UF6PracMH/src/main/java/Visual/EliminarVisual.java
+++ M06UF6PracMH/src/main/java/Visual/EliminarVisual.java
@@ -14,7 +14,24 @@ import org.bson.Document;
  *
  * @author Adrix
  */
+/**
+ *
+ * La clase EliminarVisual contiene un método público estático llamado
+ * eliminarRemot,
+ *
+ * que elimina un repositorio remoto dado su identificador.
+ */
 public class EliminarVisual {
+
+    /**
+     *
+     * Este método elimina un repositorio remoto dada su colección y base de
+     * datos MongoDB.
+     *
+     * @param coleccio la colección MongoDB que contiene el repositorio remoto.
+     * @param bbdd la base de datos MongoDB que contiene la colección del
+     * repositorio remoto.
+     */
     public static void eliminarRemot(MongoCollection<Document> coleccio, MongoDatabase bbdd) {
         Scanner in = new Scanner(System.in);
         System.out.println("Dame el identificador del repositorio remoto.");
