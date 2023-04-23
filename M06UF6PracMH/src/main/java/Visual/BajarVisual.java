e3ca8341a63aa192288e4e91b5c8bcccc52559fc
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2023-04-23 20:58:18 +0200

    Documentar en javadoc

--- M06UF6PracMH/src/main/java/Visual/BajarVisual.java
+++ M06UF6PracMH/src/main/java/Visual/BajarVisual.java
@@ -1,29 +1,3 @@
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Visual;
-
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Scanner;
-import org.bson.Document;
-import org.bson.types.Binary;
-
-/**
- *
- * @author Adrix
- */
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -51,8 +25,27 @@ import org.bson.types.Binary;
  *
  * @author Adrix
  */
+/**
+ *
+ * Clase para bajar documentos desde una colección de MongoDB a un repositorio
+ * local.
+ */
 public class BajarVisual {
 
+    /**
+     *
+     * Método para bajar un documento desde una colección de MongoDB a un
+     * repositorio local.
+     *
+     * @param coleccion La colección de MongoDB de la que se quiere bajar un
+     * documento.
+     *
+     * @param db La base de datos de MongoDB en la que se encuentra la
+     * colección.
+     *
+     * @throws IOException Si se produce un error al realizar la operación de
+     * bajada.
+     */
     public static void bajarRemot(String coleccion, MongoDatabase db) throws IOException {
         Scanner in = new Scanner(System.in);
         // Comprobar si existe la colección
@@ -85,82 +78,10 @@ public class BajarVisual {
                 System.out.println("El nombre de la carpeta no coincide con el del repositorio remoto");
             } else {
                 carpetaCorrecta = true;
-                Pull(coleccion, db, ruta);
+                Bajar.Pull(coleccion, db, ruta);
             }
 
         }
 
     }
-
-    public static void Pull(String coleccion, MongoDatabase db, String ruta) throws IOException {
-        Scanner in = new Scanner(System.in);
-        Path rutaFinal = Paths.get(ruta);
-
-        // Seleccionar documentos a bajar por nombre
-        MongoCollection<Document> coleccionMongo = db.getCollection(coleccion);
-
-        Document documento = new Document();
-        boolean nombreCorrecto = false;
-
-        while (!nombreCorrecto) {
-            System.out.println("Dame el nombre del documento que quieres bajar:");
-            String nombreDocumento = in.nextLine();
-
-            documento = coleccionMongo.find(new Document("nom", nombreDocumento)).first();
-
-            if (documento == null) {
-                System.out.println("El documento no existe en la colección.");
-            } else {
-                nombreCorrecto = true;
-
-                // Comprobar si el archivo existe en la ruta local
-                String rutacompleta = rutaFinal.toString() + "\\" + nombreDocumento;
-                File archivoLocal = new File(rutacompleta);
-                if (archivoLocal.exists()) {
-                    // Comprobar si la fecha de creación del archivo local es anterior a la fecha de modificación del remoto
-                    Date fechaRemoto = documento.getDate("Fecha de modificación");
-                    Date fechaLocal = new Date(archivoLocal.lastModified());
-
-                    if (fechaLocal.before(fechaRemoto)) {
-                        System.out.println("El archivo local es anterior al archivo remoto. ¿Desea hacer force?");
-                        System.out.println("1. Sí");
-                        System.out.println("0. No");
-
-                        int opcion = in.nextInt();
-                        in.nextLine();
-
-                        if (opcion == 1) {
-                            // Sobrescribir archivo local
-                            try ( OutputStream out = new FileOutputStream(archivoLocal)) {
-                                String contenidoString = (String) documento.get("contenido");
-                                out.write(contenidoString.getBytes());
-                                System.out.println("El archivo se ha sobrescrito correctamente.");
-                            }catch(Exception e){
-                                System.out.println("Error a la hora de descargar el archivo");
-                            }
-                        } else if (opcion == 0) {
-                            System.out.println("La operación se ha cancelado.");
-                        } else {
-                            System.out.println("Opción inválida.");
-                        }
-                    }else{
-                        System.out.println("El archivo es mas nuevo que el subido");
-                    }
-                    
-                } else {
-                    // Descargar archivo
-                    try ( OutputStream out = new FileOutputStream(archivoLocal)) {
-                        String contenidoString = (String) documento.get("contenido");
-                        out.write(contenidoString.getBytes());
-                        System.out.println("El archivo se ha descargado correctamente.");
-                    }catch(Exception e){
-                        System.out.println("Error a la hora de descargar el archivo");
-                    }
-                }
-            }
-        }
-    }
-
-
-
 }
