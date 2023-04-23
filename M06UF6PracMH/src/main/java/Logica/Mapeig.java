4402a69b234d08d2d3feeebb9526dd80c336bba7
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2023-04-23 20:17:03 +0200

    documentado en javadoc

--- M06UF6PracMH/src/main/java/Logica/Mapeig.java
+++ M06UF6PracMH/src/main/java/Logica/Mapeig.java
@@ -12,29 +12,60 @@ import org.bson.Document;
  *
  * @author Adrix
  */
+/**
+ *
+ * La clase Mapeig proporciona métodos para convertir datos entre objetos
+ * Archivodata y objetos Document de MongoDB.
+ */
 public class Mapeig {
-    public static Document setArchivoToDocument(Archivodata e){
-        
+
+    /**
+     *
+     * Convierte un objeto Archivodata en un objeto Document.
+     *
+     * @param e Archivodata que se desea convertir.
+     *
+     * @return Document con los datos del Archivodata.
+     */
+    public static Document setArchivoToDocument(Archivodata e) {
+
         Document ret = new Document("nom", e.getNom())
                 .append("tiempo", e.getTiempo())
                 .append("contenido", e.getContenido());
-        
+
         return ret;
     }
-    public static Archivodata getArchivoFromDocument(Document d)
-    {
-         Archivodata ret = new Archivodata();
-         
-         ret.setNom(d.getString("nom"));
-         ret.setTiempo(d.getDate("tiempo"));
-         ret.setContenido(d.getString("contenido"));
-         
-         return ret;
-        
+
+    /**
+     *
+     * Convierte un objeto Document de MongoDB en un objeto Archivodata.
+     *
+     * @param d Document de MongoDB que se desea convertir.
+     *
+     * @return Archivodata con los datos del Document.
+     */
+    public static Archivodata getArchivoFromDocument(Document d) {
+        Archivodata ret = new Archivodata();
+
+        ret.setNom(d.getString("nom"));
+        ret.setTiempo(d.getDate("tiempo"));
+        ret.setContenido(d.getString("contenido"));
+
+        return ret;
+
     }
+
+    /**
+     *
+     * Actualiza los campos de tiempo y contenido de un objeto Document de
+     * MongoDB a partir de un objeto Archivodata.
+     *
+     * @param e Archivodata con los datos a actualizar.
+     *
+     * @return Document con los datos actualizados.
+     */
     public static Document updateDocument(Archivodata e) {
 
-        
         Document update = new Document("$set", new Document("tiempo", e.getTiempo())
                 .append("contenido", e.getContenido()));
 
